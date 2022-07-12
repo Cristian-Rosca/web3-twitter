ac575a968aaadd8b94a72c8302e19fc332a277c5
Cristian Rosca <cristianrrosca@gmail.com>
2022-07-12 18:49:20 +0200

    add state variables to Decentratwitter contract

--- contracts/Decentratwitter.sol
+++ contracts/Decentratwitter.sol
@@ -6,5 +6,18 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract Decentratwitter is ERC721URIStorage {
+    uint256 public tokenCount;
+    uint256 public postCount;
+
+    mapping(uint256 => Post) public posts;
+    // address -> NFT id
+    mapping(address => uint256) public profiles;
+
+    struct Post {
+        uint256 id; 
+        string hash;
+        uint256 tipAmount;
+        address payable author;
+    }
     constructor() ERC721("Decentratwitter", "DAPP") {}
 }
