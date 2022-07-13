11101ff253355100fba0cae3a463979d37e4c00e
Cristian Rosca <cristianrrosca@gmail.com>
2022-07-13 16:58:45 +0200

    add uploadPost, tipPostOwner and tests

--- test/Decentratwitter.test.js
+++ test/Decentratwitter.test.js
@@ -38,4 +38,56 @@ describe("Decentratwitter", function () {
             expect(await decentratwitter.tokenURI(2)).to.equal(URI);
         });
     })
+
+    describe('Setting Profiles', async () => {
+        it("Should allow users to select which NFT they own to represent their profile", async function () {
+            // user1 mints another nft
+            await decentratwitter.connect(user1).mint(URI)
+            // By default the users profile is set to their last minted nft.
+            expect(await decentratwitter.profiles(user1.address)).to.equal(2);
+            // user 1 sets profile to first minted nft
+            await decentratwitter.connect(user1).setProfile(1)
+            expect(await decentratwitter.profiles(user1.address)).to.equal(1);
+            // FAIL CASE //
+            // user 2 tries to set their profile to nft number 2 owned by user 1
+            await expect(
+              decentratwitter.connect(user2).setProfile(2)
+            ).to.be.revertedWith("Must own the nft you want to select as your profile");
+          });
+    })
+
+    describe('Tipping post', async () => {
+        it("Should allow users to tip posts and track each posts tip amount", async function () {
+            // user1 uploads a post
+            await decentratwitter.connect(user1).uploadPost(postHash)
+            // track user1 balance before their post gets tipped
+            const initAuthorBalance = await ethers.provider.getBalance(user1.address)
+            // set tip amount to 1 ether
+            const tipAmount = ethers.utils.parseEther("1")
+            //user 2 tips user1's post
+            await expect(decentratwitter.connect(user2).tipPostOwner(1, {value: tipAmount}))
+                .to.emit(decentratwitter, "PostTipped")
+                .withArgs(
+                    1,
+                    postHash,
+                    tipAmount,
+                    user1.address
+                )
+                // check that tipAmount has been updated from struct
+                const post = await decentratwitter.posts(1)
+                expect(post.tipAmount).to.equal(tipAmount)
+                // check that user1 received funds
+                const finalAuthBalance = await ethers.provider.getBalance(user1.address)
+                expect(finalAuthBalance).to.equal(initAuthorBalance.add(tipAmount))
+                // Fail case 1
+                // user 2 tries to tip post that does not exist
+                await expect(
+                    decentratwitter.connect(user2).tipPostOwner(2)
+                ).to.be.revertedWith("Invalid post id");
+                // user 1 tries to tip their own post
+                await expect(
+                    decentratwitter.connect(user1).tipPostOwner(1)
+                ).to.be.revertedWith("Cannot tip your own post");
+        })
+    })
 });
